1057b0e26ec9244819205d35413b50c252923d81
Deathlive <armen1298@gmail.com>
2019-09-23 11:44:44 +0400

    fix-3

--- src/client/utils/socket.ts
+++ src/client/utils/socket.ts
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 
-const publicUrl = process.env.PUBLIC_URL || 'http://localhost:3003';
+// const publicUrl = process.env.PUBLIC_URL || 'http://localhost:3003';
 
-const rootSocket = io(publicUrl, { transports: ['websocket'] });
+const rootSocket = io(window.location.origin, { transports: ['websocket'] });
 
 rootSocket.on('reconnect_attempt', () => {
   rootSocket.io.opts.transports = ['polling', 'websocket'];
