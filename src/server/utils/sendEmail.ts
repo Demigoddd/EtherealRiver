f6afe56f92a6a09520def4ab489f37ebae0275ff
Deathlive <armen1298@gmail.com>
2019-09-29 17:29:43 +0400

    Added SMTP transport for email.

--- src/server/utils/sendEmail.ts
+++ src/server/utils/sendEmail.ts
@@ -1,15 +1,16 @@
 import nodeMailer from "nodemailer";
+import smtpTransport from "nodemailer-smtp-transport";
 import config from "./config";
 
 export default (emailBody: any) =>
   new Promise((resolve, reject) => {
-    nodeMailer.createTransport({
+    nodeMailer.createTransport(smtpTransport({
       service: config.email.service,
       auth: {
         user: config.email.emailUsername,
         pass: config.email.emailPassword
       }
-    })
+    }))
       .sendMail(emailBody, (error: any, info: any) => {
         if (error) {
           return reject(error);
